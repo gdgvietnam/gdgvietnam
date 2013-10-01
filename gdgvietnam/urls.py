146a90df9c59de7d8d9b399085b9ba3e20052069
root <root@prod1.gdgvietnam.org>
2013-10-01 08:05:11 +0000

    Production ready

--- gdgvietnam/urls.py
+++ gdgvietnam/urls.py
@@ -1,6 +1,7 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from mezzanine.core.views import direct_to_template
 
@@ -89,3 +90,6 @@ urlpatterns = patterns("",
 # pages can use JS, CSS and images.
 handler404 = "mezzanine.core.views.page_not_found"
 handler500 = "mezzanine.core.views.server_error"
+
+#urlpatterns += staticfiles_urlpatterns()
+
