93ffd9c68e53456247c9e0fde61233f71bc717a4
Thai Ly Cuong <thailycuong@cogini.com>
2013-11-07 21:51:44 +0700

    Add all events to homepage

--- gdgvietnam/context_processors.py
+++ gdgvietnam/context_processors.py
@@ -1,4 +1,4 @@
-from mezzanine_events.models import EventContainer,Event
+from mezzanine_events.models import EventContainer
 def all_events(request):
 	event_container = EventContainer.objects.all()[0]
-	return {'events': event_container.future_events}
+	return {'events': event_container.events}
